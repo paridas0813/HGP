0e7948b93a5ec849d05bfa66e2e08601b5331867
zhangzhongyang <zhangzhongyang@xiaomi.com>
2024-01-17 14:53:38 +0800

    add system function

--- gapic/src/main/com/google/gapid/views/TracerDialog.java
+++ gapic/src/main/com/google/gapid/views/TracerDialog.java
@@ -553,15 +553,14 @@ public class TracerDialog {
         }, new GridData(SWT.FILL, SWT.FILL, true, false));
 
         fileLabel = createLabel(outGroup, "File Name*:");
-        //解决systemtrace崩溃问题
-          // if(friendlyName ==null)
-          // {
-          //   friendlyName = "hello";
-          // }
-        // file = withLayoutData(createTextbox(outGroup, formatTraceName(friendlyName)),
-        //     new GridData(SWT.FILL, SWT.FILL, true, false));
-
-        file = withLayoutData(createTextbox(outGroup, friendlyName),new GridData(SWT.FILL, SWT.FILL, true, false));
+        //暂时解决systemtrace崩溃问题
+          if(friendlyName ==null)
+          {
+            friendlyName = "hello";
+          }
+        file = withLayoutData(createTextbox(outGroup, formatTraceName(friendlyName)),
+            new GridData(SWT.FILL, SWT.FILL, true, false));
+       // file = withLayoutData(createTextbox(outGroup, friendlyName),new GridData(SWT.FILL, SWT.FILL, true, false));
 
         requiredFieldMessage = withLayoutData(
             createLabel(this, "Please fill out required information (labeled with *)."),
@@ -690,7 +689,7 @@ public class TracerDialog {
         combo.setContentProvider(ArrayContentProvider.getInstance());
         //暂时取消 system profiler
         combo.setInput(TraceType.values());
-        combo.setInput(new  TraceType[]{TraceType.Vulkan,TraceType.ANGLE});
+        combo.setInput(new  TraceType[]{TraceType.System, TraceType.Vulkan,TraceType.ANGLE});
         combo.setSelection(new StructuredSelection(deflt));
         return combo;
       }
@@ -940,6 +939,8 @@ public class TracerDialog {
       protected String formatTraceName(String name) {
         TraceType type = getSelectedType();
         DeviceCaptureInfo dev = getSelectedDevice();
+        if(type == null)
+            return "system";
         String ext = type.getFileExtension(dev);
         // For ANGLE captures include "_angle" in trace name
         String angle = (type == TraceType.ANGLE) ? ANGLE_STRING : "";
